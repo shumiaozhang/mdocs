f7c554d215ac99fe21099d82efe7d522e0eb1f6e
Sapling <shumiao_zhg@163.com>
2023-07-31 13:18:50 +0800

    更新时间

--- .vitepress/config.js
+++ .vitepress/config.js
@@ -9,8 +9,19 @@ export default {
     srcDir: 'src',
     lastUpdated: true,
     cleanUrls: true,
+    lang: 'zh',
     themeConfig: {
         nav,
         sidebar,
+        lastUpdatedText: '上次更新',
+        docFooter: {
+            prev: '上一页',
+            next: '下一页'
+        },
+        outline: {
+            level: "deep", // 右侧大纲标题层级
+            label: "目录", // 右侧大纲标题文本配置
+        },
+        returnToTopLabel: '返回顶部',
     },
 }
