369cb16ada552f13f45f6694385f30cac3b7aa00
Sapling <shumiao_zhg@163.com>
2023-08-05 22:12:37 +0800

    auto update blog

--- bin/autoDeploy.bat
+++ bin/autoDeploy.bat
@@ -1,3 +1,5 @@
+git rm docs/.vitepress/dist
+
 git pull
 git add .
 git commit -m "auto update blog"
@@ -10,13 +12,8 @@ git init
 git add -A
 git commit -m "auto construct blog"
 
-<<<<<<< HEAD
+
 git config --global http.sslVerify "false"
 
 
 git push -f https://github.com/shumiaozhang/mdocs.git master:gh-pages
-=======
-
-
-git push -f https://gitee.com/zhangshumiao/mdocs.git master:gh-pages
->>>>>>> 4898750deea4ba2b7c069087aa4c84dc24b82398
