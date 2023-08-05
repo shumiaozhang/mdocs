e28f5337f35e4b2e3868ea657591c6ebd5c58259
Sapling <shumiao_zhg@163.com>
2023-08-05 19:19:48 +0800

    auto update blog

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -7,22 +7,28 @@ export default {
     title: 'Sapling',
     description: 'Just playing around.',
     // srcDir: 'src',
-    lastUpdated: true,
+    // lastUpdated: true, // 更新时间
     cleanUrls: true,
     lang: 'zh',
     ignoreDeadLinks: true,
+    head: [
+        [
+            'link', { rel: 'icon', href: '../public/little-tree.ico', crossorigin: '' }
+        ],
+    ],
     themeConfig: {
-        nav,
-        sidebar,
-        lastUpdatedText: '上次更新',
-        docFooter: {
-            prev: '上一页',
-            next: '下一页'
-        },
-        outline: {
-            level: "deep", // 右侧大纲标题层级
-            label: "目录", // 右侧大纲标题文本配置
-        },
-        returnToTopLabel: '返回顶部',
+    logo: '/little-tree.png',
+    nav,
+    sidebar,
+    lastUpdatedText: '上次更新',
+    docFooter: {
+        prev: '上一页',
+        next: '下一页'
+    },
+    outline: {
+        level: "deep", // 右侧大纲标题层级
+        label: "目录", // 右侧大纲标题文本配置
+    },
+    returnToTopLabel: '返回顶部',
     },
 }
--- docs/index.md
+++ docs/index.md
@@ -1,4 +1,4 @@
 
-![小树苗](img.png)
+![小树苗](public/little-tree.png)
 
 欢迎来到小树苗的世界!
